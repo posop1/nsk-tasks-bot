4a4de24257792b8bb38a2dd0988e65428a3d3279
posop1 <artur.yakhin52@gmail.com>
2023-08-18 15:28:36 +0500

    feat: logger levels and formats

--- src/libs/logger/logger.ts
+++ src/libs/logger/logger.ts
@@ -1,5 +1,26 @@
 import pino from "pino";
 
-export const logger = pino({});
+const levels = {
+	emerg: 80,
+	alert: 70,
+	crit: 60,
+	error: 50,
+	warn: 40,
+	notice: 30,
+	info: 20,
+	debug: 10
+};
+
+export const logger = pino({
+	level: process.env.PINO_LOG_LEVEL || "info",
+	customLevels: levels,
+	useOnlyCustomLevels: true,
+	formatters: {
+		level: (label) => {
+			return { level: label.toUpperCase() };
+		}
+	},
+	timestamp: pino.stdTimeFunctions.isoTime
+});
 
 // TODO: add logger levels on logs
