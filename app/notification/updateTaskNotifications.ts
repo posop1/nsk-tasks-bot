b15bb8ce215082fce2f3af59d0003ce62dcaeaef
posop1 <artur.yakhin52@gmail.com>
2023-10-10 10:28:41 +0500

    test fetch

--- app/notification/updateTaskNotifications.ts
+++ app/notification/updateTaskNotifications.ts
@@ -1,8 +1,8 @@
-// import { Bot, Context, Api, RawApi } from "grammy";
-// import { config } from "../config/config";
-// import { logger } from "../../libs/logger/logger";
+import { Bot, Context, Api, RawApi } from "grammy";
+import { config } from "../config/config";
+import { logger } from "../../libs/logger/logger";
 
-// export const updateTaskNotifications = (CHATID: string, bot: Bot<Context, Api<RawApi>>) => {
-// 	const INTERVAL = +config.INTERVAL;
-// 	logger.info("Update Notifications started");
-// };
+export const updateTaskNotifications = (CHATID: string, bot: Bot<Context, Api<RawApi>>) => {
+	const INTERVAL = +config.INTERVAL;
+	logger.info("Update Notifications started");
+};
