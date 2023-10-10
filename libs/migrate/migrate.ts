068f69b835feb83119d7046c6954b4721a341052
posop1 <artur.yakhin52@gmail.com>
2023-10-10 10:26:27 +0500

    test fetch

--- libs/migrate/migrate.ts
+++ libs/migrate/migrate.ts
@@ -1,12 +1,23 @@
 import fs from "fs";
 import { logger } from "../logger/logger";
-import { fetch } from "../../app/api/api";
+// import { fetch } from "../../app/api/api";
 import { IDataFile } from "../../app/types/dataFile";
 import { storage } from "../storage/fileStorage";
+import { config } from "../../app/config/config";
+import { IProjects } from "../../app/types/projects";
+import { IBoard } from "../../app/types/board";
+
+const { ACCESSTOKEN, APP_URL } = config;
 
 const setCount = () => {
 	setTimeout(async () => {
-		const boards = await fetch.getAllBoards();
+		// const boards = await fetch.getAllBoards();
+		const boards: IBoard[] = await fetch(`${APP_URL}/api/projects`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${ACCESSTOKEN}`
+			}
+		}).then((res) => res.json());
 
 		if (!boards) {
 			return logger.error("Migrate - boards not found");
@@ -28,7 +39,13 @@ const setCount = () => {
 
 const migrateDbFile = async () => {
 	const dir = `${__dirname}/../../db`;
-	const projects = await fetch.getProjects();
+	// const projects = await fetch.getProjects();
+	const projects: IProjects = await fetch(`${APP_URL}/api/projects`, {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${ACCESSTOKEN}`
+		}
+	}).then((res) => res.json());
 
 	if (!projects) {
 		return logger.error("Migrate - projects not found");
